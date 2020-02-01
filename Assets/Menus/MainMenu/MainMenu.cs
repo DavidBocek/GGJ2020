1312a16a42fdd45b001446bee286d1add37fe415
David Bocek <dmbocek@gmail.com>
2020-02-01 10:42:10 -0800

    some assets

---
+++ Assets/Menus/MainMenu/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	public Button playButton;
+	public string playScene;
+
+    void Start()
+    {
+		playButton.onClick.AddListener( OnPlayButtonClick );
+	}
+
+    void Update()
+    {
+        
+    }
+
+	void OnPlayButtonClick()
+	{
+		if ( playScene == "" )
+			return;
+
+		SceneManager.LoadScene( playScene );
+	}
+}
