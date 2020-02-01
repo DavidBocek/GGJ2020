8e3f571db3fefa6b05b4cde0a688fdf67d396982
David Bocek <dmbocek@gmail.com>
2020-02-01 13:12:29 -0800

    selection stuff

---
+++ Assets/Player/Script/Selectable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Selectable : MonoBehaviour
+{
+	public GameObject selectionRing;
+
+	public bool isSelected;
+	
+
+	public void OnSelectionUpdate( bool selected )
+	{
+		if ( selected )
+		{
+			selectionRing.SetActive( true );
+		}
+		else
+		{
+			selectionRing.SetActive( false );
+		}
+	}
+
+	void OnEnable()
+	{
+		Selection.selectables.Add( this );
+	}
+
+	void OnDisable()
+	{
+		Selection.selectables.Remove( this );
+	}
+
+}
