ee7fad4bba24bf124a1814d8c175ba29150f8b34
David Bocek <dmbocek@gmail.com>
2020-02-01 13:18:00 -0800

    more project settings

---
+++ Assets/Player/Script/PlayerCommands.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCommands : MonoBehaviour
+{
+
+
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+		UpdateOrders();
+	}
+
+	private void UpdateOrders()
+	{
+		if ( !Input.GetButtonDown( "Order" ) )
+			return;
+
+		if ( Selection.selectables.Count == 0 )
+			return;
+
+
+	}
+}
