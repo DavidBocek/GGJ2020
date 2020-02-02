313ec97fdd08c34edb41a887f7950d8a40994e9a
David Bocek <dmbocek@gmail.com>
2020-02-02 14:45:09 -0800

    brian

--- Assets/Actors/Enemies/Script/EnemyBrain.cs
+++ Assets/Actors/Enemies/Script/EnemyBrain.cs
@@ -6,6 +6,15 @@ public class EnemyBrain : MonoBehaviour
 {
 	public List<Transform> spawnLocs;
 
+	[System.Serializable]
+	public class EnemySpawnerSpawn
+	{
+		public GameObject spawnerObj;
+		public float time;
+		[HideInInspector]
+		public bool completed = false;
+	}
+	public List<EnemySpawnerSpawn> spawnerSpawns;
 	
 
     void Start()
@@ -15,6 +24,17 @@ public class EnemyBrain : MonoBehaviour
 
     void Update()
     {
-        
+        foreach( EnemySpawnerSpawn spawner in spawnerSpawns )
+		{
+			if ( spawner.completed )
+				continue;
+
+			if (Time.time > spawner.time)
+			{
+				Transform loc = spawnLocs[Random.Range( 0, spawnLocs.Count )];
+				Instantiate( spawner.spawnerObj, loc.position, Quaternion.identity );
+				spawnLocs.Remove( loc );
+			}
+		}
     }
 }
--- Assets/UI/HUD/Turret/AddTurretButton.cs
+++ Assets/UI/HUD/Turret/AddTurretButton.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class AddTurretButton : MonoBehaviour
 {
 	public MoneyCounter moneyCounter;
+	public Text turretCost;
 
     void Start()
     {
 		GetComponent<Button>().onClick.AddListener( OnButtonClick );
+		turretCost.text = PlayerCommands.Get().addTurretCost.ToString();
 	}
 
     void Update()
