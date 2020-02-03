75cb752896999fd157a860f1d0221cf60e478b5e
ddperera <ritsoko@gmail.com>
2020-02-02 19:28:20 -0500

    add music

---
+++ Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( GameObject.FindGameObjectsWithTag( "MusicPlayer" ).Length > 1 )
+        { 
+            Destroy( gameObject );
+        }
+        else
+        {
+            GameObject.DontDestroyOnLoad( gameObject );
+            gameObject.transform.position = Camera.main.transform.position;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
